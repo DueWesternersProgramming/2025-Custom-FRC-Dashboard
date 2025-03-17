21fc11b5bc9849ea6631a0c7fcf291003669342b
hjmcphail76 <hjmcphail76@gmail.com>
2025-03-17 11:07:38 -0400

    swap to shiv actions

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import time
+import sys
+
+
+def main():
+    """Entry point for the Shiv-packaged dashboard"""
+    import tkinter_interface  # Ensure this runs inside Shiv
 
 
 if __name__ == "__main__":
-    import tkinter_interface
+    sys.exit(main())
