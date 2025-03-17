e71efb3e2587975fa86b7514e3e2f928af632ed7
hjmcphail76 <hjmcphail76@gmail.com>
2025-03-17 12:28:24 -0400

    Revert "swap to shiv actions"
    This reverts commit 21fc11b5bc9849ea6631a0c7fcf291003669342b.

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import time
-import sys
-
-
-def main():
-    """Entry point for the Shiv-packaged dashboard"""
-    import tkinter_interface  # Ensure this runs inside Shiv
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    import tkinter_interface
